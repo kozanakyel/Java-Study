6b559625831675b3c91af642c77b0633af67a6d0
kozanakyel <39781380+kozanakyel@users.noreply.github.com>
2020-03-28 14:56:28 +0300

    Add files via upload

---
+++ LabAngleDegree.java
@@ -0,0 +1,108 @@
+import java.util.Scanner;
+
+/**
+ * @author : Uğur Akyel
+ * @date   : 27/03/2020
+ * */
+
+public class Solution_2 {
+    public static void main(String[] args){
+        final int LOW_DEGREE = 0;
+        final int HIGH_DEGREE = 360;
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter an Angle value; ");
+        int degree = input.nextInt();
+
+        System.out.println("Enter an Line1 length: ");
+        double line1Length = input.nextDouble();
+
+        System.out.println("Enter Line2 Length: ");
+        double line2Length = input.nextDouble();
+
+        try{
+            Line line1 = new Line(line1Length);
+            Line line2 = new Line(line2Length);
+            Angle angle = new Angle(degree);
+
+            double result = vectorProduct(line1, line2, angle);
+            System.out.println("The vector product is: " + result);
+        }
+        catch (IllegalArgumentException ex){
+            while(degree < LOW_DEGREE || degree > HIGH_DEGREE){
+                if(degree < LOW_DEGREE)
+                    degree += HIGH_DEGREE;
+                else
+                    degree -= HIGH_DEGREE;
+            }
+
+            System.out.println(ex);
+        }
+        finally {
+            System.out.println("The Angle value is: " + degree);
+        }
+    }
+
+    public static double vectorProduct(Line line1, Line line2, Angle angle){
+        return line1.getLength() * line2.getLength() * Math.cos(angle.getDegree());
+    }
+
+}
+
+class Angle{
+    public final int LOW_DEGREE = 0;
+    public final int HIGH_DEGREE = 360;
+    private int degree;
+
+    public Angle() throws IllegalArgumentException{
+        this(0);
+    }
+
+    public Angle(int degree) throws IllegalArgumentException{
+        setDegree(degree);
+    }
+
+    public int getDegree(){
+        return degree;
+    }
+
+    public void setDegree(int degree)
+            throws IllegalArgumentException{
+        if(degree >= LOW_DEGREE && degree <= HIGH_DEGREE)
+            this.degree = degree;
+        else
+            throw new IllegalArgumentException(
+                    "Degree must be 0 and 360 interval!");
+    }
+
+    public String toString(){
+        return "The angle value is " + degree + ".";
+    }
+}
+
+class Line{
+    private double length;
+
+    public Line(){
+    }
+
+    public Line(double length) throws IllegalArgumentException{
+        setlength(length);
+    }
+
+    public double getLength(){
+        return length;
+    }
+
+    public void setlength(double length){
+        if(length > 0)
+            this.length = length;
+        else
+            throw new IllegalArgumentException(
+                    "Length must be positive");
+    }
+
+    public String toString(){
+        return "This line's Length is " + length + ".";
+    }
+}
---
+++ LabTryCatch.java
@@ -0,0 +1,91 @@
+/**
+ * @author : Uğur Akyel
+ * @date   : 27/03/2020
+ * */
+
+public class Solution_1 {
+    public static void main(String[] args){
+
+        try {
+            /**
+             * 1)In this point; Exception in
+             * NullPointerException: String null
+             * */
+            final String comparison = null;
+            System.out.println(comparison.equalsIgnoreCase(
+                    "Compare me!"));
+        }
+        catch (NullPointerException ex3){
+            System.out.println("Null pointer exception");
+        }
+
+        try {
+            /**
+             * 2)In this point; Exception in
+             * IllegalArgumentException: Angle must not 370
+             * */
+            Angle angle1 = new Angle(370);
+        }
+        catch (IllegalArgumentException ex4){
+            System.out.println("degree cannot be greater than 360");
+        }
+
+        try{
+            /**3) In this point; Exception in
+             * ArrayIndexOutOfBoundsException:
+             *Index 10 out of bounds for length 10
+             */
+            int[] list = new int[10];
+            for(int i = 0; i < list.length; i++)
+                list[i] = i;
+            list[10] = 2;
+        }
+        catch(ArrayIndexOutOfBoundsException ex1){
+            System.out.println("Array index bound exception");
+        }
+
+        try {
+            /**4)In this point; Exception in
+             * ArithmeticException: / by zero
+             */
+            int first = 4;
+            int second = 0;
+            int result = first / second;
+        }
+        catch (ArithmeticException ex2){
+            System.out.println("Divisor cannot be zero..");
+        }
+    }
+
+}
+
+class Angle{
+    public final int LOW_DEGREE = 0;
+    public final int HIGH_DEGREE = 360;
+    private int degree;
+
+    public Angle() throws IllegalArgumentException{
+        this(0);
+    }
+
+    public Angle(int degree) throws IllegalArgumentException{
+        setDegree(degree);
+    }
+
+    public int getDegree(){
+        return degree;
+    }
+
+    public void setDegree(int degree)
+            throws IllegalArgumentException{
+        if(degree >= LOW_DEGREE && degree <= HIGH_DEGREE)
+            this.degree = degree;
+        else
+            throw new IllegalArgumentException(
+                    "Degree must be 0 and 360 interval!");
+    }
+
+    public String toString(){
+        return "The angle value is " + degree + ".";
+    }
+}
